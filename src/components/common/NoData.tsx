814ebb5e23ef6247ffcdd072444e01f0ab0b64b8
kunikaesp <kunika@espsofttech.com>
2025-11-14 01:22:36 +0530

    design changes dashboard

--- src/components/common/NoData.tsx
+++ src/components/common/NoData.tsx
@@ -1,12 +1,25 @@
 import { SearchX } from 'lucide-react'
 
 
-const NoData = () => {
+const NoData = ({col} ) => {
   return (
-    <div style={{height:'40vh',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',width:'100%'}}>
-        <SearchX style={{ fontSize: 50, color: '#ccc' }} />
-        <h3 style={{marginTop:'20px'}}>Data Not Found</h3>
-    </div>
+    <>  
+      {col ?
+        <tr>
+          <td colSpan={col}>
+            <div style={{ height: '40vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+              <SearchX style={{ fontSize: 50, color: '#ccc' }} />
+              <h3 style={{ marginTop: '20px' }}>Data Not Found</h3>
+            </div>
+          </td>
+        </tr >
+        :
+        <div style={{ height: '40vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+          <SearchX style={{ fontSize: 50, color: '#ccc' }} />
+          <h3 style={{ marginTop: '20px' }}>Data Not Found</h3>
+        </div>
+      }
+    </>
   )
 }
 
